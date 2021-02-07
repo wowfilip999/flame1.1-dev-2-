5afb3acc7d10380a06eb0898ced064b0768a6299
wowfilip999 <67916726+wowfilip999@users.noreply.github.com>
2021-02-07 16:35:39 +0100

    update clickmodes pynput finaly jay

--- click-qt.py
+++ click-qt.py
@@ -1,9 +1,12 @@
 from PyQt5 import QtWidgets
 import customclick as clickspeed
+import clickqtlook as look
 import os
 import time
 import pyautogui
-from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QSize, QProcess
+from PyQt5.QtGui import QImage, QFont, QIcon
+
 t = "slow"
 class style:
   o = "lightgray"
@@ -15,6 +18,7 @@ slow = QtWidgets.QPushButton(t, parent=form)
 fast = QtWidgets.QPushButton("fast", parent=form)
 custom = QtWidgets.QPushButton("custom", parent=form)
 text = QtWidgets.QLabel("select modes", parent=form)
+customset = QtWidgets.QPushButton("",parent=form)
 form.setGeometry(700, 700, 700, 700)
 form.setWindowTitle("flame > click modes")
 
@@ -58,6 +62,12 @@ custom.setStyleSheet("QPushButton"
                    "}"
                )
 
+customset.setStyleSheet("QPushButton"
+                       "{"
+                       "background-color: lightgray;"
+                       "}"
+
+                    )
 def customclick():
  if clickspeed.click == "click1":
    pyautogui.clicks()
@@ -343,15 +353,37 @@ def c():
  time.sleep(0.1)
  pyautogui.click()
 
-
+import bind as bd
 
 def slowmode():
- time.sleep(2)
- c()
- c()
- c()
- c()
- c()
+ from pynput import keyboard
+
+ def on_press(key):
+    pyautogui.click()
+    time.sleep(0.2)
+    pyautogui.click()
+
+ def on_release(key):
+    bt = bd.bind
+    t = "left"
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.left:
+        # Stop listener
+        return False
+
+ # Collect events until released
+ with keyboard.Listener(
+       on_press=on_press,
+       on_release=on_release) as listener:
+     listener.join()
+
+ # ...or, in a non-blocking fashion:
+ listener = keyboard.Listener(
+     on_press=on_press,
+     on_release=on_release)
+ listener.start()
+
 
 
 
@@ -376,7 +408,11 @@ def infofunct():
  print("test")
 
 
+def test():
+ os.system("python3 opt1.py")
 
+
+customset.clicked.connect(lambda: test())
 slow.clicked.connect(lambda: slowmode())
 fast.clicked.connect(lambda: fastfunct())
 info.clicked.connect(lambda: infofunct())
@@ -389,11 +425,16 @@ fast.resize(500, 90)
 custom.resize(500, 90)
 slow.move(400, 100)
 fast.move(400, 190)
+customset.move(900, 100)
+customset.resize(80, 90)
 custom.move(400, 280)
-text.setFont(QFont("Arial", 16))
-slow.setFont(QFont("Arial", 13))
-fast.setFont(QFont("Arial", 13))
-custom.setFont(QFont("Arial", 13))
+text.setFont(QFont("Arial", look.fontsize))
+slow.setFont(QFont(look.font, look.fontsize))
+fast.setFont(QFont(look.font, look.fontsize)) # before 13
+custom.setFont(QFont(look.font, look.fontsize))
+
+customset.setIcon(QIcon('settings.png'))
+
 text.move(550, 40)
 info.move(720, 55)
 form.show() 
