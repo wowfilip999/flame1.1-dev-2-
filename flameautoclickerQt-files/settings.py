f6fc6b743477adda30c0ba590e3aeb530191d1ec
wowfilip999 <67916726+wowfilip999@users.noreply.github.com>
2021-02-07 16:37:31 +0100

    Delete settings.py

--- flameautoclickerQt-files/settings.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-z = QtWidgets.QApplication
-forr = QtWidgets.QWidget()
-t = QtWidgets.QPushButton("legit settings", parent=forr)
-formular.setGeometry(700, 700, 700, 700)
-
-formular.show()
-
-z.exec_()
