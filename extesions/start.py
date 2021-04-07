d9f10c2d86a60442de08bf7c9c35b5263ef791ba
wowfilip999 <67916726+wowfilip999@users.noreply.github.com>
2021-04-07 14:01:42 +0200

    Delete start.py

--- extesions/start.py
+++
@@ -1,9 +0,0 @@
-import os
-f = open("start", "f")
-print(f.read()) import os
-f = open("start", "f")
-print(f.read()) import os
-f = open("start", "f")
-print(f.read()) import os
-f = open("log.txt", "f")
-print(f.read()) 
